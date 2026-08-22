beea64fa970c550dc53f58a9264cad79f58bd9a2
IMROVICH <main.imr2015@gmail.com>
2026-08-22 22:14:31 +0700

    Create OutdateNotification.lua

---
+++ LimoHub/OutdateNotification.lua
@@ -0,0 +1,28 @@
+local Players = game:GetService("Players")
+local StarterGui = game:GetService("StarterGui")
+
+local player = Players.LocalPlayer
+local nick = player.Name .. "!"
+local nick2 = player.Name
+
+task.wait(1)
+
+-- Уведомление
+StarterGui:SetCore("SendNotification", {
+	Title = "Your script is outdated",
+	Text = nick2 .. " ,please restart this script!",
+	Duration = 7,
+})
+task.wait(0.5)
+-- Звук через PlayerGui
+local PlayerGui = player:WaitForChild("PlayerGui")
+local sound = Instance.new("Sound")
+sound.SoundId = "rbxassetid://131661992591924" -- ID стандартного звука уведомления
+sound.Volume = 1.5
+sound.Name = "OutdateNotificationSound"
+sound.Parent = PlayerGui
+sound:Play()
+task.wait(1)
+sound.Ended:Connect(function()
+	sound:Destroy()
+end)
